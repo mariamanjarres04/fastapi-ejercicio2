79b682205146176c25432bc567e562d5c4ba6da8
ApoloKn <polosilverajainer@hotmial.com>
2026-09-21 10:38:39 -0500

    agregamos catalogo d eproductos

--- main.py
+++ main.py
@@ -12,3 +12,46 @@ class Producto(BaseModel):
     precio: float
     categoria: str
     quantity: int
+
+productos = [
+    {
+        "id": 1,
+        "nombre": "Teclado mecánico",
+        "precio": 180000,
+        "categoria": "electrónica",
+        "quantity": 10
+    },
+    {
+        "id": 2,
+        "nombre": "Mouse inalámbrico",
+        "precio": 85000,
+        "categoria": "electrónica",
+        "quantity": 15
+    },
+    {
+        "id": 3,
+        "nombre": "Silla ergonómica",
+        "precio": 650000,
+        "categoria": "muebles",
+        "quantity": 5
+    },
+    {
+        "id": 4,
+        "nombre": "Teclado inalámbrico",
+        "precio": 120000,
+        "categoria": "electrónica",
+        "quantity": 8
+    },
+    {
+            "id": 5,
+            "nombre": "Memoria usb",
+            "precio": 47000,
+            "categoria": "electrónica",
+            "quantity": 8
+        }
+
+]
+
+@app.get("/productos")
+def obtener_productos():
+    return productos
